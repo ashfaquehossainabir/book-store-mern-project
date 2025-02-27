61d2ac5ebc5d1d36fbbfa15e2b57801488a17d7f
Ashfaque Hossain Abir <109417020+ashfaquehossainabir@users.noreply.github.com>
2025-02-27 16:43:18 +0600

    Updated

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
   useEffect(() => {
     setLoading(true)
 
-    axios.get('https://book-store-mern-project-backend-ddvi.onrender.com/books')
+    axios.get('http://localhost:5555/books')
           .then((res) => {
             setBooks(res.data.Data)
             setLoading(false)
